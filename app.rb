cf2aadd7562473f70928ee52487157800b9d619f
Razona <razona0718@gmail.com>
2018-09-04 14:36:17 +0900

    コードの整形

--- app.rb
+++ app.rb
@@ -2,46 +2,40 @@ require 'sinatra'
 require 'osc-ruby'
 
 
-get '/' do
-  erb :index
-end
+def osc_send(msg)
 
-get '/zahyou/:num' do
-
-    num = params[:num]
-
-    # ローカルホストの UDPポート : 10000 宛にOSCメッセージを送信するOSCクライアントのインスタンスを作成
-    client = OSC::Client.new('localhost', 10000)
-
-    # OSCメッセージを作成
-    # 第一引数にOSCアドレス、以降に送信する内容を指定する
-    message = OSC::Message.new('/system', num)
-
-    # OSCメッセージの送信
-    client.send(message)
-
-end
-
-get '/clear' do
-
-  # ローカルホストの UDPポート : 10000 宛にOSCメッセージを送信するOSCクライアントのインスタンスを作成
+  # ローカルホストの UDPポート : 3333 宛にOSCメッセージを送信するOSCクライアントのインスタンスを作成
   client = OSC::Client.new('localhost', 10000)
 
   # OSCメッセージを作成
   # 第一引数にOSCアドレス、以降に送信する内容を指定する
-  message = OSC::Message.new('/system', 'clear')
+  message = OSC::Message.new('/system', msg)
 
   # OSCメッセージの送信
   client.send(message)
+
+end
+
+#起動時に接続成功メッセージ
+osc_send('接続成功')
+
+
+
+#一応作ったルートとindex
+get '/' do
+  erb :index
 end
 
 
-# ローカルホストの UDPポート : 3333 宛にOSCメッセージを送信するOSCクライアントのインスタンスを作成
-client = OSC::Client.new('localhost', 10000)
+get '/zahyou/:num' do
+#座標に関するパラメーターを代入
+    num = params[:num]
+#osc送信
+    osc_send(num)
+end
 
-# OSCメッセージを作成
-# 第一引数にOSCアドレス、以降に送信する内容を指定する
-message = OSC::Message.new('/system', '接続成功')
 
-# OSCメッセージの送信
-client.send(message)
+get '/clear' do
+  #osc送信
+  osc_send('clear')
+end
